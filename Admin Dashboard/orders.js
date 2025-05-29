5887dfb616acfcb156e324144fed070d738f197c
Akshay Suresh <akshaysuresh1117@gmail.com>
2025-05-29 23:05:57 +0530

    Add files via upload

---
+++ Admin Dashboard/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'React Tutorial',
+        productNumber: '87637',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Angular Tutorial',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
